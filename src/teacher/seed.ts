0239a4e4ebb08865ad76b1db8df80d9801f5122f
FStriewski <fstriewski@outlook.com>
2018-04-03 15:30:39 +0200

    Disable seeds for now

--- src/teacher/seed.ts
+++ src/teacher/seed.ts
@@ -1,30 +1,75 @@
-import Teacher from './entity'
+// import Teacher from './entity'
+// import Student from '../student/entity'
+// import Evaluation from '../evaluation/entity'
+// import Batch from '../batch/entity'
+// import { createConnection, Connection } from 'typeorm'
+// import { DefaultNamingStrategy } from 'typeorm/naming-strategy/DefaultNamingStrategy'
+// import { NamingStrategyInterface } from 'typeorm/naming-strategy/NamingStrategyInterface'
+// import { snakeCase } from 'typeorm/util/StringUtils'
+
+// class CustomNamingStrategy extends DefaultNamingStrategy implements NamingStrategyInterface {
+
+//     tableName(targetName: string, userSpecifiedName: string): string {
+//         return userSpecifiedName ? userSpecifiedName : snakeCase(targetName) + 's';
+//     }
+
+//     columnName(propertyName: string, customName: string, embeddedPrefixes: string[]): string {
+//         return snakeCase(embeddedPrefixes.concat(customName ? customName : propertyName).join("_"));
+//     }
+
+//     columnNameCustomized(customName: string): string {
+//         return customName;
+//     }
+
+//     relationName(propertyName: string): string {
+//         return snakeCase(propertyName);
+//     }
+// }
 
 
 
-const seed = async () => {
-    console.log('Seed start.')
+// const config = {
+//     type: "postgres",
+//     url: process.env.DATABASE_URL || 'postgres://postgres:secret@localhost:5432/postgres',
+//     entities: [
+//         Student,
+//         Teacher,
+//         Evaluation,
+//         Batch,
+//     ],
+//     synchronize: true,
+//     logging: true,
+//     namingStrategy: new CustomNamingStrategy()
+// }
 
 
-    try {
 
-        const teacher =  {
-            email: "tim@teacher.tt",
-            password: "12345678"
-        }
+// const seed = async () => {
 
-        const { password, ...rest } = teacher
-        const entity = Teacher.create(rest)
-        await entity.setPassword(password)
-        return entity.save()
+//     const conn = await createConnection(config);
 
-        // console.log('Seed end')
+//     console.log('Seed start.')
 
-        // return
 
-        }
-    catch(e){
-        return { e: e.message }
-    }
-}
-seed();
+//     try {
+
+//         const teacher =  {
+//             email: "tim2@teacher.tt",
+//             password: "12345678"
+//         }
+
+//         const { password, ...rest } = teacher
+//         const entity = Teacher.create(rest)
+//         await entity.setPassword(password)
+//         return entity.save()
+
+//         // console.log('Seed end')
+
+//         // return
+
+//         }
+//     catch(e){
+//         return { e: e.message }
+//     }
+// }
+// seed();

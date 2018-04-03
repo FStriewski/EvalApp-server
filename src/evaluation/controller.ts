3974f0860926a0fececd69e46c589f787a1ffb97
FStriewski <fstriewski@outlook.com>
2018-04-03 15:36:40 +0200

    Add evaluation controller (CRU)

---
+++ src/evaluation/controller.ts
@@ -0,0 +1,43 @@
+import { JsonController, Get, NotFoundError, Param, Post, Body, Put, Delete } from "routing-controllers";
+import Evaluation from './entity'
+
+
+@JsonController()
+export default class EvaluationController {
+
+    @Get('/evaluations')
+    async getAllEvaluations() {
+        return await Evaluation.find()
+    }
+    @Get('/evaluations/:id([0-9]+)')
+    async getSingleEvaluation(
+        @Param("id") id: number
+    ) {
+        const evaluation = await Evaluation.findOneById(id)
+        if (!evaluation) throw new NotFoundError("No evaluation found")
+        return evaluation
+    }
+
+    // Needs user connection
+    @Post('/evaluations')
+    async createEvaluation(
+        @Body() body: Evaluation
+    ) {
+        const evaluation = await Evaluation.create(body).save()
+        return evaluation
+    }
+
+
+    @Put('/evaluations/:id([0-9]+)')
+    async updateEvaluation(
+        @Param("id") id: number,
+        @Body() update: Evaluation
+    ) {
+        const evaluation = await Evaluation.findOneById(id)
+        if (!evaluation) throw new NotFoundError("Evaluation not found")
+
+        return Evaluation.merge(evaluation, update).save()
+    }
+
+
+}

934d5eba78f9b60aca7bd48b0577b45370bb37ca
FStriewski <fstriewski@outlook.com>
2018-04-03 13:48:41 +0200

    Add authorization checker, currentUser checker

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,14 @@
 import 'reflect-metadata'
-import {createKoaServer, Action} from 'routing-controllers'
+import { createKoaServer, Action, BadRequestError} from 'routing-controllers'
 import TeacherController from './teacher/controller'
 import EvaluationController from './evaluation/controller'
 import BatchController from './batch/controller'
 import StudentController from './student/controller'
+import Teacher from './teacher/entity'
+import { verify } from './jwt'
+
+
+
 
 export default createKoaServer({
   controllers: [
@@ -11,15 +16,32 @@ export default createKoaServer({
     EvaluationController,
     BatchController,
     StudentController,
-   
-   ]
-  // authorizationChecker: (action: Action) => {
-  //   const header: string =
-  //   action.request.headers.authorization
-  //   if (header && header.startsWith('Bearer')) {
-  //     const [ , token ] = header.split(' ')
-  //     return !!(token && verify(token))
-  //   }
-  //   return false
-  // }
+   ],
+  authorizationChecker: (action: Action) => {
+    const header: string = action.request.headers.authorization
+    if (header && header.startsWith('Bearer ')) {
+      const [, token] = header.split(' ')
+
+      try {
+        return !!(token && verify(token))
+      }
+      catch (e) {
+        throw new BadRequestError(e)
+      }
+    }
+
+    return false
+  },
+  currentUserChecker: async (action: Action) => {
+    const header: string = action.request.headers.authorization
+    if (header && header.startsWith('Bearer ')) {
+      const [, token] = header.split(' ')
+
+      if (token) {
+        const { id } = verify(token)
+        return Teacher.findOneById(id)
+      }
+    }
+    return undefined
+  }
 })
